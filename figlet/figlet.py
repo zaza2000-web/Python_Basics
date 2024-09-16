12a6cacfcfea77d4af8427c30ca95d922b485c10
Zaza <Zazuka2000badzagua@gmail.com>
2024-09-16 20:40:04 +0200

    fullComplete figlet

---
+++ figlet/figlet.py
@@ -0,0 +1,22 @@
+import sys
+import random
+from pyfiglet import Figlet
+
+def main():
+    figlet = Figlet()
+    fonts = figlet.getFonts()
+    if len(sys.argv) == 1:
+        text = input("Write: ")
+        random_font = random.choice(fonts)
+        figlet.setFont(font = random_font)
+        print(figlet.renderText(text))
+    elif len(sys.argv) == 3 and  sys.argv[1] == "--f":
+        font_name = sys.argv[2]
+        if font_name in fonts:
+            figlet.setFont(font = font_name)
+            text = input("Write: ")
+            print(figlet.renderText(text))
+       
+
+
+main()
